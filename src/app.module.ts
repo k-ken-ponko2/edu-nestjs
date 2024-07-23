23d4b45ac13ddf3e97e96482db68859b496b51e4
k.ken <kiyose.kensuke@revoneo.com>
2024-07-23 23:07:35 +0900

    2nd

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ModelModule } from './model/model.module';
+import { ResultModule } from './result/result.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, ModelModule, ResultModule],
   controllers: [AppController],
   providers: [AppService],
 })
